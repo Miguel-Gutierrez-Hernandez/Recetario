1f86bb59d60976d1488ef992c71e110ca1be3f15
Miguel-elec <mgutierrezhe.inf@upsa.es>
2026-05-27 23:47:27 +0200

    wikipedia

--- src/boti/main.py
+++ src/boti/main.py
@@ -1,13 +1,14 @@
 import speech_recognition as sr
-import pyttsx3
-import pywhatkit
+import pyttsx3, pywhatkit, wikipedia, datetime, keyboard
+from pygame import mixer
 from config import NAME
 
 listener = sr.Recognizer()
 engine = pyttsx3.init()
 
 voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[0].id)
+engine.setProperty('voice', voices[92].id)
+
 
 def talk(text):
     engine.say(text)
@@ -19,6 +20,7 @@ def listen():
     try:
         with sr.Microphone() as source:
             print("Escuchando...")
+            talk("Escuchando...")
             audio = listener.listen(source)
 
             command = listener.recognize_google(
@@ -42,15 +44,18 @@ def listen():
 def run():
     command = listen()
 
-    if not command:
-        return
-
     if 'reproduce' in command:
         song = command.replace('reproduce', '').strip()
-
         talk('Reproduciendo ' + song)
-
         pywhatkit.playonyt(song)
+
+    elif 'busca' in command:
+        topic = command.replace('busca', '').strip()
+        wikipedia.set_lang('es')
+        talk('Buscando ' + topic + ' en Wikipedia')
+        info = wikipedia.summary(topic, sentences=1) # sentences=1 para obtener solo el primer párrafo, puedes ajustar este número según tus necesidades
+        print(info)
+        talk(info)
 
 if __name__ == '__main__':
     run()
